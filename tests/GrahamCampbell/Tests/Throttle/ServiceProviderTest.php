63d158e06daf412dc9cfb659ca7dc5728c8bf4e7
Graham Campbell <graham@mineuk.com>
2013-12-26 21:31:10 +0000

    Test refactor

--- tests/GrahamCampbell/Tests/Throttle/ServiceProviderTest.php
+++ tests/GrahamCampbell/Tests/Throttle/ServiceProviderTest.php
@@ -27,17 +27,7 @@ use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
  * @license    https://github.com/GrahamCampbell/Laravel-Throttle/blob/develop/LICENSE.md
  * @link       https://github.com/GrahamCampbell/Laravel-Throttle
  */
-abstract class ServiceProviderTest extends AbstractTestCase
+class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
-
-    /**
-     * Get the service provider class.
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass()
-    {
-        return 'GrahamCampbell\Throttle\ThrottleServiceProvider';
-    }
 }
