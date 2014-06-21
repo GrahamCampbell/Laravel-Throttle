6f4411d7632b7ef1fd8c24ea6802dc5e8be53024
Graham Campbell <graham@mineuk.com>
2014-06-21 16:57:19 +0100

    Injection test

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -30,4 +30,9 @@ use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
 class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
+
+    public function testThrottleIsInjectable()
+    {
+        $this->assertIsInjectable('GrahamCampbell\Throttle\Throttle');
+    }
 }
