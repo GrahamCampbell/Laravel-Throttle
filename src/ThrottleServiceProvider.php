c927dfbcd0d653588acfa2d6d5fbbd100ca40a12
Graham Campbell <graham@alt-three.com>
2015-10-04 11:34:43 +0100

    Improved lumen support

--- src/ThrottleServiceProvider.php
+++ src/ThrottleServiceProvider.php
@@ -45,6 +45,8 @@ class ThrottleServiceProvider extends ServiceProvider
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('throttle.php')]);
+        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+            $this->configure('throttle');
         }
 
         $this->mergeConfigFrom($source, 'throttle');
